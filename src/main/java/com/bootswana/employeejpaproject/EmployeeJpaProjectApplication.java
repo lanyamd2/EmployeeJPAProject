cc8ec3d16bf970ee35e8a2c156b1c0318e4fb220
lanyamd2 <laniangel12@gmail.com>
2023-05-23 15:18:53 +0100

    initial commit - empty main

---
+++ src/main/java/com/bootswana/employeejpaproject/EmployeeJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bootswana.employeejpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeJpaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootswana/employeejpaproject/EmployeeJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootswana.employeejpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
