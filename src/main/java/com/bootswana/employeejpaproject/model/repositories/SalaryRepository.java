28196a55b65f0c598ffe5ca19b860bb0ab2e776d
HusseinAw <46084220+HusseinAw@users.noreply.github.com>
2023-05-25 15:34:08 +0100

    added tests and functionality

--- src/main/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepository.java
+++ src/main/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepository.java
@@ -6,13 +6,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Repository
 public interface SalaryRepository extends JpaRepository<SalaryDTO, SalaryDTOId> {
+    @Query(value = "SELECT CAST(MIN(s.salary) AS DECIMAL(10, 2)) AS lowest_amount_paid, CAST(MAX(s.salary) AS DECIMAL(10, 2)) AS highest_amount_paid FROM employees.titles t JOIN employees.salaries s ON t.emp_no = s.emp_no WHERE t.title = :jobTitle AND YEAR(s.from_date) = :year GROUP BY t.title", nativeQuery = true)
+    Map<String, BigDecimal> getLowestAndHighestSalaryForJobTitleDuringAYear(String jobTitle, int year);
 
-    @Query(value = "SELECT AVG(s.salary) AS average_salary FROM employees.departments d JOIN employees.dept_emp de ON d.dept_no = de.dept_no JOIN employees.salaries s ON de.emp_no = s.emp_no WHERE d.dept_name = :department AND :date BETWEEN s.from_date AND s.to_date;", nativeQuery = true)
-    Map<String, Integer> getAverageSalaryForDepartmentOnGivenDate(String department, LocalDate date);
+    @Query(value = "SELECT t.title, CAST((SUM(CASE WHEN e.gender = 'M' THEN s.salary ELSE 0 END) - SUM(CASE WHEN e.gender = 'F' THEN s.salary ELSE 0 END)) / SUM(s.salary) * 100 AS DECIMAL(10, 2)) AS male_to_female_pay_gap_percentage FROM employees.employees e JOIN employees.titles t ON e.emp_no = t.emp_no JOIN employees.salaries s ON e.emp_no = s.emp_no WHERE (YEAR(s.from_date) BETWEEN :fromYear AND :toYear OR YEAR(s.to_date) BETWEEN :fromYear AND :toYear OR (YEAR(s.from_date) <= :fromYear AND YEAR(s.to_date) >= :toYear)) GROUP BY t.title ORDER BY male_to_female_pay_gap_percentage DESC", nativeQuery = true)
+    List<Object[]> getGenderPayGapPercentageBetweenTwoYearsForEachJobTitle(int fromYear, int toYear);
+
+    @Query(value = "SELECT CAST(AVG(s.salary) AS DECIMAL(10, 2)) AS average_salary FROM employees.departments d JOIN employees.dept_emp de ON d.dept_no = de.dept_no JOIN employees.salaries s ON de.emp_no = s.emp_no WHERE d.dept_name = :department AND :date BETWEEN s.from_date AND s.to_date", nativeQuery = true)
+    Map<String, BigDecimal> getAverageSalaryForDepartmentOnGivenDate(String department, LocalDate date);
 
 }
--- src/test/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepositoryTests.java
+++ src/test/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepositoryTests.java
@@ -1,8 +1,16 @@
 package com.bootswana.employeejpaproject.model.repositories;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class SalaryRepositoryTests {
@@ -11,6 +19,30 @@ class SalaryRepositoryTests {
 
     @Autowired
     SalaryRepository salaryRepository;
+
+    @Test
+    @DisplayName("Check Get Lowest And Highest Salary For Job Title During A Year")
+    void checkGetLowestAndHighestSalaryForJobTitleDuringAYear() {
+        String jobTitle = "Engineer";
+        int year = 2000;
+        Map<String, BigDecimal> salaryMap = salaryRepository.getLowestAndHighestSalaryForJobTitleDuringAYear(jobTitle, year);
+        BigDecimal lowestAmountPaid = salaryMap.get("lowest_amount_paid");
+        BigDecimal highestAmountPaid = salaryMap.get("highest_amount_paid");
+        Assertions.assertTrue(lowestAmountPaid.compareTo(highestAmountPaid) <= 0);
+    }
+
+    @Test
+    @DisplayName("Check Gender Pay Gap Percentage For Each Job Title Between Two Years")
+    void checkGenderPayGapPercentageForEachJobTitleBetweenTwoYearsIsACorrectPercentage() {
+        int fromYear = 1990;
+        int toYear = 2000;
+        List<Object[]> resultRows = salaryRepository.getGenderPayGapPercentageBetweenTwoYearsForEachJobTitle(fromYear, toYear);
+        for (Object[] row : resultRows) {
+            BigDecimal payGapPercentage = (BigDecimal) row[1];
+            Assertions.assertTrue(payGapPercentage.compareTo( new BigDecimal(100)) <= 0);
+            Assertions.assertTrue(payGapPercentage.compareTo( new BigDecimal(-100)) >= 0);
+        }
+    }
 
     @Test
     void contextLoads() {
