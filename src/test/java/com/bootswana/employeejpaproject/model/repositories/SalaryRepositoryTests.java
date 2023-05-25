c185b3c278e16ea2763b8bcc850e4623f1f83ddc
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-25 15:28:31 +0100

    Added test for average salary.

--- src/test/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepositoryTests.java
+++ src/test/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepositoryTests.java
@@ -1,8 +1,17 @@
 package com.bootswana.employeejpaproject.model.repositories;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class SalaryRepositoryTests {
@@ -16,4 +25,18 @@ class SalaryRepositoryTests {
     void contextLoads() {
     }
 
+    @Test
+    @DisplayName("checkAverageSalaryFromDepartmentGivenDate")
+    void checkAverageSalaryFromDepartmentGivenDate() {
+        BigDecimal expected = BigDecimal.valueOf(68567.95);
+        String department = "Finance";
+        LocalDate date = LocalDate.of(1995, 6, 26);
+
+        Map<String, BigDecimal> salaryMap = salaryRepository.getAverageSalaryForDepartmentOnGivenDate(department, date);
+        BigDecimal averageSalary = salaryMap.get("average_salary");
+
+        System.out.println("Average Salary: " + averageSalary + " converted to 2 decimal places.");
+
+        assertEquals(expected.doubleValue(), salaryMap.get("average_salary").doubleValue());
+    }
 }
