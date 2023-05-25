c6f3f87d87e7aa8280fb980f667fc896af138f68
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-25 15:28:53 +0100

    Fixed data type conversion.

--- src/main/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepository.java
+++ src/main/java/com/bootswana/employeejpaproject/model/repositories/SalaryRepository.java
@@ -6,13 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Map;
 
 @Repository
 public interface SalaryRepository extends JpaRepository<SalaryDTO, SalaryDTOId> {
 
-    @Query(value = "SELECT AVG(s.salary) AS average_salary FROM employees.departments d JOIN employees.dept_emp de ON d.dept_no = de.dept_no JOIN employees.salaries s ON de.emp_no = s.emp_no WHERE d.dept_name = :department AND :date BETWEEN s.from_date AND s.to_date;", nativeQuery = true)
-    Map<String, Integer> getAverageSalaryForDepartmentOnGivenDate(String department, LocalDate date);
+    @Query(value = "SELECT CAST(AVG(s.salary) AS DECIMAL (10,2)) AS average_salary FROM employees.departments d JOIN employees.dept_emp de ON d.dept_no = de.dept_no JOIN employees.salaries s ON de.emp_no = s.emp_no WHERE d.dept_name = :department AND :date BETWEEN s.from_date AND s.to_date;", nativeQuery = true)
+    Map<String, BigDecimal> getAverageSalaryForDepartmentOnGivenDate(String department, LocalDate date);
 
 }
