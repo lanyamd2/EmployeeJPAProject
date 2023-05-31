8ae6cc82b8f0db1cedd8debb50f4a5d8882f3efc
lanyamd2 <laniangel12@gmail.com>
2023-05-31 15:44:31 +0100

    added static imports for linkTo and methodOn in MainController, also added appropriate dependencies to pom.xml

--- src/main/java/com/bootswana/employeejpaproject/controllers/MainController.java
+++ src/main/java/com/bootswana/employeejpaproject/controllers/MainController.java
@@ -5,12 +5,12 @@ import com.bootswana.employeejpaproject.service.EmployeesService;
 import com.bootswana.employeejpaproject.service.SalariesService;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 public class MainController {
     //services
     private EmployeesService employeesService;
     private DepartmentsService departmentsService;
     private SalariesService salariesService;
-    //repositories
-
 }
