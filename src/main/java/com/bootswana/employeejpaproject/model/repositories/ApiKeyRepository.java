73b1aaa535f38f5a5ec75de8b03a11b4bafad7c2
M Usman Abubakr <86719130+Usman-Abubakr@users.noreply.github.com>
2023-05-31 16:27:23 +0100

    Added more queries.

--- src/main/java/com/bootswana/employeejpaproject/model/repositories/ApiKeyRepository.java
+++ src/main/java/com/bootswana/employeejpaproject/model/repositories/ApiKeyRepository.java
@@ -1,8 +1,8 @@
 package com.bootswana.employeejpaproject.model.repositories;
 
 import com.bootswana.employeejpaproject.model.dtos.ApiKeyDTO;
-import com.bootswana.employeejpaproject.model.dtos.EmployeeDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -11,5 +11,15 @@ public interface ApiKeyRepository extends JpaRepository<ApiKeyDTO, String> {
 
     @Query(value = "SELECT k.* FROM employees.api_keys k", nativeQuery = true)
     List<ApiKeyDTO> getAllApiKeys();
+
+    @Query(value = "SELECT k.access_level FROM employees.api_keys k WHERE k.api_key = :apiKey", nativeQuery = true)
+    Integer getApiAccessLevel(String apiKey);
+
+
+//    @Modifying
+//    @Query(value = "INSERT INTO employees.api_keys (api_key, access_level) VALUES (:apiKey, :accessLevel);", nativeQuery = true)
+//    void setApiKey(String apiKey, int accessLevel);
+
+
 
 }
--- src/test/java/com/bootswana/employeejpaproject/model/repositories/ApiKeyRepositoryTests.java
+++ src/test/java/com/bootswana/employeejpaproject/model/repositories/ApiKeyRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 public class ApiKeyRepositoryTests {
 
@@ -17,6 +19,26 @@ public class ApiKeyRepositoryTests {
     void checkGetAllApiKeysExist() {
         System.out.println(apiKeyRepository.getAllApiKeys());
         Assertions.assertFalse(apiKeyRepository.getAllApiKeys().isEmpty());
+    }
+
+    @Test
+    @DisplayName("Check Get API Access Level")
+    void checkGetApiAccessLevel() {
+        String key = "test123";
+        System.out.println(apiKeyRepository.getApiAccessLevel(key));
+        Assertions.assertEquals(1, apiKeyRepository.getApiAccessLevel(key));
+    }
+
+    @Test
+    @DisplayName("Check insert new api key into database")
+    void checkInsertNewApiKeyIntoDatabase() {
+
+        String key = "generated123";
+        int accessLevel = 3;
+
+        apiKeyRepository.setApiKey(key, accessLevel);
+
+        Assertions.assertEquals("[ApiKeyDTO{apiKey='generated123', accessLevel=3}]", apiKeyRepository.findById("generated123"));
     }
 
 }
