bf230156259a9d577d971e68ffe1590bf460c991
Chamara Silva <chamarasilva55@gmail.com>
2023-06-01 17:57:50 +0100

    Added endpoint methods for getting employees by dept on date and getting managers by dept chronologically

--- src/main/java/com/bootswana/employeejpaproject/controllers/MainController.java
+++ src/main/java/com/bootswana/employeejpaproject/controllers/MainController.java
@@ -3,13 +3,12 @@ package com.bootswana.employeejpaproject.controllers;
 import com.bootswana.employeejpaproject.EmployeeJpaProjectApplication;
 import com.bootswana.employeejpaproject.exception.ApiKeyNotFoundException;
 import com.bootswana.employeejpaproject.model.dtos.EmployeeDTO;
+import com.bootswana.employeejpaproject.model.dtos.IManagerProjection;
 import com.bootswana.employeejpaproject.model.repositories.EmployeeRepository;
 import com.bootswana.employeejpaproject.model.repositories.ApiKeyRepository;
-import com.bootswana.employeejpaproject.service.ApiKeyService;
-import com.bootswana.employeejpaproject.service.DepartmentsService;
-import com.bootswana.employeejpaproject.service.EmployeesService;
-import com.bootswana.employeejpaproject.service.SalariesService;
+import com.bootswana.employeejpaproject.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +18,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -83,4 +84,27 @@ public class MainController {
             return new ResponseEntity<>("Employee " + id + " not found", HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/employees")
+    public HttpEntity<?> getEmployeesByDeptOnDate(@RequestParam(name = "department") String deptName, @RequestParam(name = "date") LocalDate date, @RequestParam(name = "apiKey") String apiKey) throws ApiKeyNotFoundException {
+        //int level = apiKeyService.getAccessLevel(apiKey);
+        Optional<List<EmployeeDTO>> employees = employeesService.getEmployeesByDepartmentNameOnDate(deptName, date);
+        if(employees.isPresent()) {
+            return new ResponseEntity<>(employees.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No employees found working in the " + deptName + " department on " + Utility.getDateAsString(date), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/employees/managers")
+    public HttpEntity<?> getManagersByDeptChronologically(@RequestParam(name = "department") String deptName, @RequestParam(name = "apiKey") String apiKey) throws ApiKeyNotFoundException {
+        //int level = apiKeyService.getAccessLevel(apiKey);
+        Optional<List<IManagerProjection>> managers = employeesService.getManagersByDepartmentChronologically(deptName);
+        if(managers.isPresent()) {
+            return new ResponseEntity<>(managers.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("No managers found from the " + deptName + " department", HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
--- src/test/java/com/bootswana/employeejpaproject/controllers/MainControllerTests.java
+++ src/test/java/com/bootswana/employeejpaproject/controllers/MainControllerTests.java
@@ -1,10 +1,26 @@
 package com.bootswana.employeejpaproject.controllers;
 
+import com.bootswana.employeejpaproject.model.dtos.EmployeeDTO;
+import com.bootswana.employeejpaproject.model.dtos.IManagerProjection;
 import com.bootswana.employeejpaproject.service.DepartmentsService;
 import com.bootswana.employeejpaproject.service.EmployeesService;
 import com.bootswana.employeejpaproject.service.SalariesService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class MainControllerTests {
@@ -16,5 +32,47 @@ public class MainControllerTests {
 
     @Autowired
     SalariesService salariesService;
+
+    @Test
+    @DisplayName("test employees by valid department and date")
+    void testEmployeesEndpointByValidDeptAndDate() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<EmployeeDTO>> response = restTemplate.exchange(
+                "http://localhost:8080/employees?department=Finance&date=2000-01-01&apiKey=u4Ip9hbD7VyQqWDrrfjw16_PjtqyRJD8",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+
+    @Test
+    @DisplayName("test employees by invalid department and date")
+    void testEmployeesEndpointByInvalidDeptAndDate() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.getForEntity("http://localhost:8080/employees?department=Technology&date=2000-01-01&apiKey=u4Ip9hbD7VyQqWDrrfjw16_PjtqyRJD8", String.class);
+            fail();
+        } catch (HttpClientErrorException e) {
+            if (!e.toString().contains("No employees found working in the Technology department on 2000-01-01")){
+                fail();
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("test managers by invalid department")
+    void testManagersEndpointByInvalidDept() {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            restTemplate.getForEntity("http://localhost:8080/employees/managers?department=Technology&apiKey=u4Ip9hbD7VyQqWDrrfjw16_PjtqyRJD8", String.class);
+            fail();
+        } catch (HttpClientErrorException e) {
+            if (!e.toString().contains("No managers found from the Technology department")){
+                fail();
+            }
+        }
+    }
 
 }
